e1a112d2416e4d8e2e5897ca3de3fe15e9817447
Jan Ole Zabel <jan.zabel@posteo.de>
2022-04-06 12:15:00 +0200

    Split tests into own codefile

--- src/lib.rs
+++ src/lib.rs
@@ -169,44 +169,4 @@ impl Trie {
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::Trie;
-
-    #[test]
-    fn insert() {
-        let trie = Trie::from(["Hallo", "Hallöchen!", "Tschüs!"]);
-        let mut suffixes: Vec<String> = trie.content_with_prefix("").collect();
-        suffixes.sort();
-        assert_eq!(
-            vec![
-                String::from("Hallo"),
-                String::from("Hallöchen!"),
-                String::from("Tschüs!")
-            ],
-            suffixes
-        );
-    }
-
-    #[test]
-    fn prefix_query() {
-        let trie = Trie::from(["Hallo", "Hallöchen!", "Tschüs!"]);
-        let mut suffixes: Vec<String> = trie.content_with_prefix("Hall").collect();
-        suffixes.sort();
-        assert_eq!(
-            vec![String::from("Hallo"), String::from("Hallöchen!")],
-            suffixes
-        );
-    }
-
-    #[test]
-    fn remove_long() {
-        let mut trie = Trie::from(["Hallo", "Hallöchen!", "Tschüs!"]);
-        trie.remove("Hallöchen!");
-        let mut suffixes: Vec<String> = trie.content_with_prefix("").collect();
-        suffixes.sort();
-        assert_eq!(
-            vec![String::from("Hallo"), String::from("Tschüs!")],
-            suffixes
-        );
-    }
-}
+mod tests;
